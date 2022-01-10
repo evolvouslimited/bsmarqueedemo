be3ef9bf1c0f1c6a7927f5121d742570abc6b8fe
shailesh <shailesh.kumar@evolvous.com>
2022-01-10 22:54:55 +0530

    updated package.json

--- src/Marquee.tsx
+++ src/Marquee.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
-const Marquee: React.FC = () => <span>Testing this word on marquee html tag</span>;
+const Marquee: React.FC = () => {
+    return(
+        <span>Testing this word on marquee html tag</span>
+    );
+};
 
 export default Marquee;
